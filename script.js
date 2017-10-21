7ccf1d4ffeaacd8abca6a3ec2025a38fea356773
robertmaxwilliams <robertmaxwilliams@gmail.com>
2017-10-21 18:39:13 -0500

    can get email from login

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ firebase.auth().getRedirectResult().then(function(result) {
     var token = result.credential.accessToken;
     var email = result.user.email;
     console.log("got token", email, token)
+    $scope.user = result.user;
     // ...
   }
   // The signed-in user info.
@@ -34,3 +35,39 @@ firebase.auth().getRedirectResult().then(function(result) {
   var credential = error.credential;
   // ...
 });
+
+app.controller("ChatCtrl", ["$scope", "chatMessages",
+  // we pass our new chatMessages factory into the controller
+  function($scope, chatMessages) {
+    //$scope.user = "Guest " + Math.round(Math.random() * 100);
+
+    // we add chatMessages array to the scope to be used in our ng-repeat
+    $scope.messages = chatMessages;
+
+    // a method to create new messages; called by ng-submit
+    $scope.addMessage = function() {
+      // calling $add on a synchronized array is like Array.push(),
+      // except that it saves the changes to our database!
+      $scope.messages.$add({
+        from: $scope.user,
+        content: $scope.message
+      });
+
+      // reset the message input
+      $scope.message = "";
+    };
+
+    // if the messages are empty, add something for fun!
+    $scope.messages.$loaded(function() {
+      if ($scope.messages.length === 0) {
+        $scope.messages.$add({
+          from: "Firebase Docs",
+          content: "Hello world!"
+        });
+      }
+    });
+  }
+]);
+function makePool(name){
+  var database = firebase.uuuuu
+};
