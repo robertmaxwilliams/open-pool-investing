0e9b8d1c8a4ce10609a734a8359bcbdb5199211b
Robert <robertmaxwilliams@gmail.com>
2017-10-21 15:07:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var app = angular.module("openPool", ["firebase"]);
 
 
-
-
-
 app.controller("SampleCtrl", function($scope, $firebaseArray) {
   var ref = firebase.database().ref().child("messages");
   // create a synchronized array
-  // click on `index.html` above to see it used in the DOM!
   $scope.messages = $firebaseArray(ref);
+  // add new items to the array
+  // the message is automatically added to our Firebase database!
+  $scope.addMessage = function() {
+    $scope.messages.$add({
+      text: $scope.newMessageText
+    });
+  };
+  // click on `index.html` above to see $remove() and $save() in action
 });
