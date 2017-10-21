8727cd711f107fae2b8945be43d2b826712da6fb
Robert <robertmaxwilliams@gmail.com>
2017-10-21 14:57:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-var config = {
-  5, 2, 3
-};
-firebase.initializeApp(config);
+var app = angular.module("openPool", ["firebase"]);
 
-angular.module('ngAppDemo', []).controller('ngAppDemoController', function($scope) {
-  $scope.a = 1;
-  $scope.b = 2;
+
+
+
+
+app.controller("SampleCtrl", function($scope, $firebaseObject) {
+  var ref = firebase.database().ref().child("data");
+  // download the data into a local object
+  var syncObject = $firebaseObject(ref);
+  // synchronize the object with a three-way data binding
+  // click on `index.html` above to see it used in the DOM!
+  syncObject.$bindTo($scope, "data");
 });
