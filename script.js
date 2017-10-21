cbf823ec5e5963554791ea04fd09e76a046c242b
Robert <robertmaxwilliams@gmail.com>
2017-10-21 14:42:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var config = {
+  5, 2, 3
+};
+firebase.initializeApp(config);
+
 angular.module('ngAppDemo', []).controller('ngAppDemoController', function($scope) {
   $scope.a = 1;
   $scope.b = 2;
