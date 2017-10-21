ebbf063d0ed2d5569fb9776ece321aae71aacccb
Robert <robertmaxwilliams@gmail.com>
2017-10-21 15:44:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 var app = angular.module("openPool", ["firebase"]);
 
+// this factory returns a synchronized array of chat messages
+app.factory("chatMessages", ["$firebaseArray",
+  function($firebaseArray) {
+    // create a reference to the database location where we will store our data
+    var ref = firebase.database().ref();
 
-app.controller("SampleCtrl", function($scope, $firebaseArray) {
-  var auth = $firebaseAuth();
+    // this uses AngularFire to create the synchronized array
+    return $firebaseArray(ref);
+  }
+]);
 
-  // login with Facebook
-  auth.$signInWithPopup("google").then(function(firebaseUser) {
-    console.log("Signed in as:", firebaseUser.uid);
-  }).catch(function(error) {
-    console.log("Authentication failed:", error);
-  });
-  
-  var ref = firebase.database().ref().child("messages");
-  // create a synchronized array
-  $scope.messages = $firebaseArray(ref);
-  // add new items to the array
-  // the message is automatically added to our Firebase database!
-  $scope.addMessage = function() {
-    $scope.messages.$add({
-      text: $scope.newMessageText
+app.controller("ChatCtrl", ["$scope", "chatMessages",
+  // we pass our new chatMessages factory into the controller
+  function($scope, chatMessages) {
+    $scope.user = "Guest " + Math.round(Math.random() * 100);
+
+    // we add chatMessages array to the scope to be used in our ng-repeat
+    $scope.messages = chatMessages;
+
+    // a method to create new messages; called by ng-submit
+    $scope.addMessage = function() {
+      // calling $add on a synchronized array is like Array.push(),
+      // except that it saves the changes to our database!
+      $scope.messages.$add({
+        from: $scope.user,
+        content: $scope.message
+      });
+
+      // reset the message input
+      $scope.message = "";
+    };
+
+    // if the messages are empty, add something for fun!
+    $scope.messages.$loaded(function() {
+      if ($scope.messages.length === 0) {
+        $scope.messages.$add({
+          from: "Firebase Docs",
+          content: "Hello world!"
+        });
+      }
     });
-  };
-  // click on `index.html` above to see $remove() and $save() in action
-});
+  }
+]);
