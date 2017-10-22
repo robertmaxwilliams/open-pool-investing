bcbbb219b82999d9c3971757a69f61dec29f5c60
robertmaxwilliams <robertmaxwilliams@gmail.com>
2017-10-21 23:40:23 -0500

    this is actually going really well

--- script.js
+++ script.js
@@ -24,9 +24,35 @@ var provider = new firebase.auth.GoogleAuthProvider();
 // we pass in a username and get back their synchronized data
 app.factory("Pool", ["$firebaseObject",
   function($firebaseObject) {
+    return function() {
+      // create a reference to the database node where we will store our data
+      var ref = firebase.database().ref("pools").push();
+      //var profileRef = ref.child(username);
+
+      // return it as a synchronized object
+      return $firebaseObject(ref);
+    }
+  }
+]);
+
+// factory for reading high level lists, like "pools"
+app.factory("ObjectsList", ["$firebaseArray",
+  function($firebaseArray) {
+    return function(database_name) {
+      // create a reference to the database node where we will store our data
+      var ref = firebase.database().ref(database_name);
+
+      // return it as a synchronized object
+      return $firebaseArray(ref);
+    }
+  }
+]);
+// object factory for syncted user data 
+app.factory("User", ["$firebaseObject",
+  function($firebaseObject) {
     return function(username) {
       // create a reference to the database node where we will store our data
-      var ref = firebase.database().ref("accounts").push();
+      var ref = firebase.database().ref("users");
       var profileRef = ref.child(username);
 
       // return it as a synchronized object
@@ -35,8 +61,9 @@ app.factory("Pool", ["$firebaseObject",
   }
 ]);
 
-app.controller('myCtrl', function($scope, Pool) {
-    $scope.user = "no user";
+
+app.controller('myCtrl', function($scope, Pool, User, ObjectsList) {
+    $scope.userid = "no user";
     //login
     $scope.login = function() {
       firebase.auth().signInWithRedirect(provider);
@@ -44,12 +71,13 @@ app.controller('myCtrl', function($scope, Pool) {
     //logout
     $scope.logout = function() {
       firebase.auth().signOut().then(function() {
-        $scope.user = "log out";
+        $scope.userid = "log out";
         $scope.$apply()
       }, function(error) {
         console.log(error);
       });
     };
+
     //login callback
     firebase.auth().getRedirectResult().then(function(result) {
       console.log("got result")
@@ -59,26 +87,43 @@ app.controller('myCtrl', function($scope, Pool) {
         var email = result.user.email;
         console.log("got token", email, token)
         $scope.user = result.user.email;
-        $scope.userid = result.user.email.substring(0, result.user.email.indexOf("@"));
-        $scope.$apply()
+        //extract email prefix to use as unique ID TODO: don't do this
+        $scope.userid = email.substring(0, email.indexOf("@"));
+        //create synced user for submitting user info
+        $scope.user = User($scope.userid);
+        $scope.user.key = $scope.userid;
+
+        //fill up with data in admin-side format
+        $scope.user.token = token;
+        $scope.user.foo = "bar";
+        $scope.user.screen_name = result.user.displayName;
+        $scope.user.picture = result.user.photoURL;
+        $scope.user.email_address = result.user.email;
+        $scope.user.bank_usd_balance = 50;
+        $scope.user.bank_btc_balance = 2;
+        $scope.user.bank_ether_balance = 10;
+        $scope.user.$save().then(function(){
+          console.log("Your user stuff:");
+          }).catch(function(error){alert('Error!');})
+        console.log($scope.user);
+        $scope.$apply();
       }
-      // The signed-in user info.
-      var user = result.user;
     }).catch(function(error) {
       console.log(error);
     });
 
+    $scope.newpool = Pool();
     //save user's new pool
     $scope.saveNewPool = function() {
-          $scope.pool = Pool($scope.userid);
-          $scope.pool.$save().then(function() {
-            alert('Pool saved!');
-          }).catch(function(error) {
-            alert('Error!');
-          });
-        };
+        $scope.newpool.$save().then(function() {
+          alert('Pool saved!');
+        }).catch(function(error) {
+          alert('Error!');
+        });
+    $scope.newpool = Pool();
+    };
+    
 
     //read existing pools into pools variable
-    $scope.pools = 
-
+    $scope.pools = ObjectsList("pools");
 });
