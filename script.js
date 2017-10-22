2296a293e5243c239c96be3e6509a3d23860d311
robertmaxwilliams <robertmaxwilliams@gmail.com>
2017-10-21 21:52:42 -0500

    I think I know what I'm doing now

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+var config = {
+  apiKey: "AIzaSyBdC5IFAhQx7m8LDPg8NZln7rPU4QwyQRQ",
+  authDomain: "open-pool.firebaseapp.com",
+  databaseURL: "https://open-pool.firebaseio.com",
+  projectId: "open-pool",
+  storageBucket: "open-pool.appspot.com",
+  messagingSenderId: "569437181618"
+};
+firebase.initializeApp(config);
 var app = angular.module("openPool", ["firebase"]);
+
 
 // let's create a re-usable factory that generates the $firebaseAuth instance
 app.factory("Auth", ["$firebaseAuth",
@@ -9,65 +19,66 @@ app.factory("Auth", ["$firebaseAuth",
 
 var provider = new firebase.auth.GoogleAuthProvider();
 
-function authorize() {
-  firebase.auth().signInWithRedirect(provider);
-}
 
-firebase.auth().getRedirectResult().then(function(result) {
-  console.log("got result")
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    var email = result.user.email;
-    console.log("got token", email, token)
-    $scope.user = result.user;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+// a factory to create a re-usable profile object
+// we pass in a username and get back their synchronized data
+app.factory("Pool", ["$firebaseObject",
+  function($firebaseObject) {
+    return function(username) {
+      // create a reference to the database node where we will store our data
+      var ref = firebase.database().ref("accounts").push();
+      var profileRef = ref.child(username);
 
-app.controller("ChatCtrl", ["$scope", "chatMessages",
-  // we pass our new chatMessages factory into the controller
-  function($scope, chatMessages) {
-    //$scope.user = "Guest " + Math.round(Math.random() * 100);
-
-    // we add chatMessages array to the scope to be used in our ng-repeat
-    $scope.messages = chatMessages;
-
-    // a method to create new messages; called by ng-submit
-    $scope.addMessage = function() {
-      // calling $add on a synchronized array is like Array.push(),
-      // except that it saves the changes to our database!
-      $scope.messages.$add({
-        from: $scope.user,
-        content: $scope.message
-      });
-
-      // reset the message input
-      $scope.message = "";
-    };
-
-    // if the messages are empty, add something for fun!
-    $scope.messages.$loaded(function() {
-      if ($scope.messages.length === 0) {
-        $scope.messages.$add({
-          from: "Firebase Docs",
-          content: "Hello world!"
-        });
-      }
-    });
+      // return it as a synchronized object
+      return $firebaseObject(profileRef);
+    }
   }
 ]);
-function makePool(name){
-  var database = firebase.uuuuu
-};
+
+app.controller('myCtrl', function($scope, Pool) {
+    $scope.user = "no user";
+    //login
+    $scope.login = function() {
+      firebase.auth().signInWithRedirect(provider);
+    };
+    //logout
+    $scope.logout = function() {
+      firebase.auth().signOut().then(function() {
+        $scope.user = "log out";
+        $scope.$apply()
+      }, function(error) {
+        console.log(error);
+      });
+    };
+    //login callback
+    firebase.auth().getRedirectResult().then(function(result) {
+      console.log("got result")
+      if (result.credential) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        var email = result.user.email;
+        console.log("got token", email, token)
+        $scope.user = result.user.email;
+        $scope.userid = result.user.email.substring(0, result.user.email.indexOf("@"));
+        $scope.$apply()
+      }
+      // The signed-in user info.
+      var user = result.user;
+    }).catch(function(error) {
+      console.log(error);
+    });
+
+    //save user's new pool
+    $scope.saveNewPool = function() {
+          $scope.pool = Pool($scope.userid);
+          $scope.pool.$save().then(function() {
+            alert('Pool saved!');
+          }).catch(function(error) {
+            alert('Error!');
+          });
+        };
+
+    //read existing pools into pools variable
+    $scope.pools = 
+
+});
